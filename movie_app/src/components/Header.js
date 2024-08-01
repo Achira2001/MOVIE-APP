ab2013cfbf1731a13285a4adaf35afc768e4fae4
Achira2001 <achira.20220024@iit.ac.lk>
2024-08-02 03:44:15 +0530

    update files

--- movie_app/src/components/Header.js
+++ movie_app/src/components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import { NavLink } from 'react-router-dom'
+import userIcon from '../assets/user.png'
+
 
 const Header = () => {
+
+    const navigation = [
+        {
+            label : "TV Shows",
+            href : "tv"
+        },
+        {
+            label : "Movies",
+            href : "movie"
+        }
+    ]
+
   return (
     <header className = "fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-75">
-        <div className = 'container mx-auto px-2 flex items-center'>
+        <div className = 'container mx-auto px-2 flex items-center h-full'>
             <div>
                 <img
                     src = {logo}
@@ -12,6 +27,22 @@ const Header = () => {
                     width = {120}
                 />
             </div>
+
+            <nav className='flex items-center gap-1 ml-5'>
+                {
+                    navigation.map((nav,index) =>{
+                        return(
+                            <div>
+                                
+                                <NavLink key = {nav.label} to = {nav.href} className = {({isActive}) => 'px-2 hover:text-neutral-100 ${isActive && "text-neutral-100"}'}>
+                                    {nav.label}
+                                </NavLink>
+                            </div>
+                        )
+                    })
+                }
+            </nav>
+
         </div>
     </header>
   )
