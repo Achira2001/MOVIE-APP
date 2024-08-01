89bc52ee4d1a67909d301eb1e37c7197c1cae6c0
Achira2001 <achira.20220024@iit.ac.lk>
2024-08-02 04:14:06 +0530

    updatr files

--- movie_app/src/components/Header.js
+++ movie_app/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import { NavLink } from 'react-router-dom'
 import userIcon from '../assets/user.png'
-
+import { IoSearchOutline } from "react-icons/io5";
 
 const Header = () => {
 
@@ -43,8 +43,11 @@ const Header = () => {
                 }
             </nav>
 
-            <div className='ml-auto'>
-                <div className='w-9 h-9 rounded-full overflow-hidden cursor-pointer'>
+            <div className='ml-auto flex items-center gap-4'>
+                <div className="text-4xl text-white">
+                <IoSearchOutline/> 
+                </div>
+                <div className='w-9 h-9 rounded-full overflow-hidden cursor-pointer active:scale-50 transition-all'>
                     <img
                         src={userIcon}
                         width="w-full h-full"
