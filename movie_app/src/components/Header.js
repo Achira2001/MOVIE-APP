e67e474d89c9323ddd31c7b9eb895ad9f0459e1a
Achira2001 <achira.20220024@iit.ac.lk>
2024-08-02 10:41:32 +0530

    update files

--- movie_app/src/components/Header.js
+++ movie_app/src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import logo from '../assets/logo.png'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import userIcon from '../assets/user.png'
 import { IoSearchOutline } from "react-icons/io5";
+
 
 const Header = () => {
 
@@ -24,16 +25,21 @@ const Header = () => {
         navigate('/search?q=${searchInput}')
     },[searchInput])
 
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+
   return (
     <header className = "fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-75">
         <div className = 'container mx-auto px-3 flex items-center h-full'>
-            <div>
+            <Link>
                 <img
                     src = {logo}
                     alt = {logo}
                     width = {120}
                 />
-            </div>
+            </Link>
 
             <nav className='hidden lg:flex items-center gap-1 ml-5'>
                 {
@@ -51,11 +57,11 @@ const Header = () => {
             </nav>
 
             <div className='ml-auto flex items-center gap-5'>
-                <form className='flex items-center gap-2'>
+                <form className='flex items-center gap-2' onSubmit={handleSubmit}>
                     <input
                         type="text"
                         placeholder="Search here..."
-                        className="bg-transparent px-4 py-1 outline-none border-none"
+                        className="bg-transparent px-4 py-1 outline-none border-none hidden lg:block"
                         onChange={(e)=>setSearchInput(e.target.value)}
                         value={searchInput}
                     />
